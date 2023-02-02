0c32852d033d7a6834deaa7eac33c6e177078f24
Ian Lewis <ianmlewis@gmail.com>
2023-02-02 06:51:22 +0000

    Fix doc ingestion
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- ingest-docs.sh
+++ ingest-docs.sh
@@ -3,4 +3,4 @@
 GUACSEC_HOME="$(go env GOPATH)/src/github.com/guacsec"
 export GUACSEC_HOME
 
-"${GUACSEC_HOME}"/guac/bin/guacone files --gdbuser neo4j --gdbpass s3cr3t ./docs/cyclonedx
+"${GUACSEC_HOME}"/guac/bin/guacone files --gdbuser neo4j --gdbpass s3cr3t ./docs
